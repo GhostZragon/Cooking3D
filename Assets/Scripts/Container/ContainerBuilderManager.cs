d5a7b2aab91a925680b7f2bde1dbdbab70fa1eca
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2024-06-08 17:53:19 +0700

    Backup

--- Assets/Scripts/Container/ContainerBuilderManager.cs
+++ Assets/Scripts/Container/ContainerBuilderManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using EasyButtons;
 using UnityEditor;
 using UnityEngine;
 // [FilePath("SomeSubFolder/StateFile.foo", FilePathAttribute.Location.PreferencesFolder)]
@@ -8,6 +9,22 @@ using UnityEngine;
 public class ContainerBuilderManager : ScriptableObject
 {
     public List<FoodContainer> foodList;
+    [Button]
+    private void LoadAllFoodPrefab()
+    {
+        string[] guids = AssetDatabase.FindAssets("l:food");
+        foreach (var guid in guids)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(guid);
+            var food = AssetDatabase.LoadAssetAtPath<Food>(path);
+            Debug.Log(food.name);
+            Debug.Log("Path " + path);
+            // if (foodData != null)
+            // {
+            //     foodsData.Add(foodData);
+            // }
+        }
+    }
     [Serializable]
     public struct FoodContainer
     {
