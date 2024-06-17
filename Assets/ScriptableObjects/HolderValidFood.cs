23b4c1e751d30751b1d414cde2c80ec713d5df79
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2024-06-17 10:00:47 +0700

    WIP

--- Assets/ScriptableObjects/HolderValidFood.cs
+++ Assets/ScriptableObjects/HolderValidFood.cs
@@ -6,22 +6,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ValidFood",menuName = "Holder Valid Food")]
 public class HolderValidFood : ScriptableObject
 {
-    private Dictionary<FoodType, List<FoodState>> foodValid = new Dictionary<FoodType, List<FoodState>>();
-    public List<FoodState> FoodStateValid;
-    private List<Food> Foods;
-#if UNITY_EDITOR
-    [Button]
-    private void LoadFoodPrefabInAsset()
-    {
-        string[] guids = AssetDatabase.FindAssets("l:food");
-        Foods.Clear();
-        foreach (var guid in guids)
-        {
-            var path = AssetDatabase.GUIDToAssetPath(guid);
-            var food = AssetDatabase.LoadAssetAtPath<Food>(path);
-            // FoodStates.Add(InitFoodState(food));
-            // Foods.Add(new FoodValid(food));
-        }
-    }
-#endif
+    public List<CookwareType> cookwareType;
+    public List<FoodType> FoodTypes;
 }
