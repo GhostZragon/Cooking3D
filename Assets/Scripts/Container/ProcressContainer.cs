6ffa6ca087995704881f3a3975e56ba10e993452
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2024-06-19 10:34:26 +0700

    WIP

--- Assets/Scripts/Container/ProcressContainer.cs
+++ Assets/Scripts/Container/ProcressContainer.cs
@@ -1,35 +1,17 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 public class ProcressContainer : HolderAbstract
 {
     [SerializeField] private FoodState foodStateWantToChange;
 
- 
-    public override void ExchangeItems(HolderAbstract player)
+    [Button]
+    private void Convert()
     {
-        if (!CanProcess(player)) return;
-        var foodData = FoodManager.instance.GetFoodData(player.GetFoodType(),foodStateWantToChange);
+        if (food == null) return; 
+        var foodData = FoodManager.instance.GetFoodData(this.food.GetFoodType(),foodStateWantToChange);
         if (foodData == null) return;
-        var food = player.GetFood();
         food.SetData(foodData);
         food.SetModel();
-        player.SwapFoodTwoWay(this);
-    }
-
-    private bool CanProcess(HolderAbstract player)
-    {
-        return !player.IsContainCookware() && player.IsContainFood();
-    }
-}
-
-public class ExchangeStrategyTest : FoodExchangeStrategy
-{
-    public bool CanExchange(HolderAbstract holder1, HolderAbstract holder2)
-    {
-        return false;
-    }
-
-    public void Exchange(HolderAbstract holder1, HolderAbstract holder2)
-    {
     }
 }
