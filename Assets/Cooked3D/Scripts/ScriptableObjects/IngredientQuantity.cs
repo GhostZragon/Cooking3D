cdcebaf8a942ca5a98d23ec6b517a1473a94b106
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2024-07-09 18:48:59 +0700

    Update IngredientQuantity.cs

--- Assets/Cooked3D/Scripts/ScriptableObjects/IngredientQuantity.cs
+++ Assets/Cooked3D/Scripts/ScriptableObjects/IngredientQuantity.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 [Serializable] 
 public class IngredientQuantity
 {
+    [SerializeField] private FoodData foodData;
+    [SerializeField] private int amount;
     public IngredientQuantity(FoodData foodData, int amout)
     {
         this.foodData = foodData;
         this.amount = amout;
     }
-    [SerializeField] private FoodData foodData;
-    [SerializeField] private int amount;
+
     public int Amount { get => amount; }
     public FoodData FoodData { get => foodData; }
     public bool CanAddMore(IngredientQuantity ingredientQuantity)
